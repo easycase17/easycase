22851180bc50e00a81753799b8d10e933a094e0d
wei170 <guocheng@guochengwei.com>
2017-07-10 17:12:03 -0400

    [IOS] Add ios platform and mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  id: 'com.EasyCase-iOS',
+  name: 'easycase',
+  description: 'Easy Case',
+  author: 'EasyCase Dev Team',
+  email: 'contact@easycase.com',
+  website: 'http://easycase.com'
+});
+
+App.configurePlugin('cordova-plugin-googleplus', {
+    REVERSED_CLIENT_ID: 'com.googleusercontent.apps.44773692988-1rfcqh2196ma5jq56bc62cresma72tin'
+});
