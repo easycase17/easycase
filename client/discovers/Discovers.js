7553b78eb4e0f34ee841b24de526f6d9488a5af6
wei170 <guocheng@guochengwei.com>
2017-07-18 10:24:24 -0700

    [DISCOVERS] Update Discovers page to show three fields using buttons to switch among them

--- client/discovers/Discovers.js
+++ client/discovers/Discovers.js
@@ -3,32 +3,46 @@ Template.Discovers.onCreated(function() {
     var self = this;
     self.fieldType = new ReactiveVar( String );
     self.fieldType.set('Cases');
+    self.discovers = new ReactiveVar( Array );
     self.autorun(function() {
-        self.subscribe('cases-discover', Meteor.userId());
+        switch(self.fieldType.get()) {
+            case 'Cases':
+                self.subscribe('cases-discover', Meteor.userId());
+                self.discovers.set(Cases.find({}));
+                break;
+            case 'Lawyers':
+                self.subscribe('lawyers');
+                self.discovers.set(Lawyers.find({}));
+                break;
+            case 'Articles':
+                break;
+        }
     });
 });
 
 Template.Discovers.helpers({
     discovers: () => {
-        switch(Template.instance().fieldType.get()) {
-            case 'Cases':
-                return Cases.find({});
-            default:
-                return Cases.find({});
-        }
+        return Template.instance().discovers.get();
     },
     isCasesField: () => {
         return Template.instance().fieldType.get() == 'Cases';
+    },
+    isLawyersField: () => {
+        return Template.instance().fieldType.get() == 'Lawyers';
+    },
+    isArticlesField: () => {
+        return Template.instance().fieldType.get() == 'Articles';
     }
 });
 
-Template.DiscoversItem.helpers({
-    // @FIXME
-    findUsername: (userId) => {
-        return new Promise(function(resolve, reject) {
-            Meteor.call('users.findUser', userId, (err, res) => {
-                 resolve(res);
-            });
-        });
+Template.Discovers.events({
+    'click #discover-cases-btn': function(event, template) {
+        template.fieldType.set('Cases');
+    },
+    'click #discover-lawyers-btn': function(event, template) {
+        template.fieldType.set('Lawyers');
+    },
+    'click #discover-articles-btn': function(event, template) {
+        template.fieldType.set('Articles');
     }
 });
