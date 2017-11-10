bbd29d062b8e65fcd082cc4935fe1c016cab099e
wei170 <guocheng@guochengwei.com>
2017-11-10 17:08:59 -0500

    Add avatar and fix some styles

--- client/main.js
+++ client/main.js
@@ -1,58 +1,65 @@
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 const userLanguage = () => {
-  // If the user is logged in, retrieve their saved language
-  if (Meteor.user()) return Meteor.user().profile.language;
+    // If the user is logged in, retrieve their saved language
+    if (Meteor.user()) return Meteor.user().profile.language;
 };
 
 if (Meteor.isClient) {
-  Meteor.startup(function () {
-    _.extend(Notifications.defaultOptions, {
-      timeout: 3500
+    Meteor.startup(function () {
+        _.extend(Notifications.defaultOptions, {
+            timeout: 3500
+        });
+
+        let lang;
+
+        // URL Language takes priority
+        const urlLang = FlowRouter.getQueryParam('lang');
+        if (urlLang) {
+            lang = urlLang;
+        } else if (userLanguage()) {
+            // User language is set if no url lang
+            lang = userLanguage();
+        } else {
+            // If no user language, try setting by browser (default en)
+            const localeFromBrowser = window.navigator.userLanguage || window.navigator.language;
+            let locale = 'en';
+
+            if (localeFromBrowser.match(/en/)) locale = 'en';
+            if (localeFromBrowser.match(/zh/)) locale = 'zh';
+
+            lang = locale;
+        }
+        TAPi18n.setLanguage(lang);
+
+        // Comments setting
+        Comments.ui.config({
+            template: 'bootstrap', // or ionic, semantic-ui
+            limit: 5,
+            loadMoreCount: 10,
+            markdown: false,
+            generateAvatar: function (user, isAnonymous) {
+                if (user.profile.avatar) {
+                    return user.profile.avatar;
+                } else {
+                    return `https://ui-avatars.com/api/?name=${user.profile.firstname}+${user.profile.lastname}&background=524763&color=FFF`;
+                }
+            }
+        });
+
+        Tracker.autorun(() => {
+            // Comments content language
+            Comments.ui.setContent({
+                title: TAPi18n.__("COMMENTS.TITLE"),
+                save: TAPi18n.__("COMMENTS.SAVE"),
+                reply: TAPi18n.__("COMMENTS.REPLY"),
+                edit: TAPi18n.__("COMMENTS.EDIT"),
+                remove: TAPi18n.__("COMMENTS.REMOVE"),
+                'placeholder-textarea': TAPi18n.__("COMMENTS.PLACEHOLDER"),
+                'add-button-reply': TAPi18n.__("COMMENTS.ADD_BUTTON_REPLY"),
+                'add-button': TAPi18n.__("COMMENTS.ADD_BUTTON"),
+                'load-more': TAPi18n.__("COMMENTS.LOAD_MORE")
+            });
+        });
     });
-
-    let lang;
-
-    // URL Language takes priority
-    const urlLang = FlowRouter.getQueryParam('lang');
-    if (urlLang) {
-      lang = urlLang;
-    } else if (userLanguage()) {
-      // User language is set if no url lang
-      lang = userLanguage();
-    } else {
-      // If no user language, try setting by browser (default en)
-      const localeFromBrowser = window.navigator.userLanguage || window.navigator.language;
-      let locale = 'en';
-
-      if (localeFromBrowser.match(/en/)) locale = 'en';
-      if (localeFromBrowser.match(/zh/)) locale = 'zh';
-
-      lang = locale;
-    }
-    TAPi18n.setLanguage(lang);
-
-    // Comments setting
-    Comments.ui.config({
-      template: 'bootstrap', // or ionic, semantic-ui
-      limit: 5,
-      loadMoreCount: 10,
-      markdown: false
-    });
-
-    Tracker.autorun(() => {
-      // Comments content language
-      Comments.ui.setContent({
-        title: TAPi18n.__("COMMENTS.TITLE"),
-        save: TAPi18n.__("COMMENTS.SAVE"),
-        reply: TAPi18n.__("COMMENTS.REPLY"),
-        edit: TAPi18n.__("COMMENTS.EDIT"),
-        remove: TAPi18n.__("COMMENTS.REMOVE"),
-        'placeholder-textarea': TAPi18n.__("COMMENTS.PLACEHOLDER"),
-        'add-button-reply': TAPi18n.__("COMMENTS.ADD_BUTTON_REPLY"),
-        'add-button': TAPi18n.__("COMMENTS.ADD_BUTTON"),
-        'load-more': TAPi18n.__("COMMENTS.LOAD_MORE")
-      });
-    });
-  });
 }
--- client/views/profile/Profile.js
+++ client/views/profile/Profile.js
@@ -17,6 +17,14 @@ Template.Profile.helpers({
     },
     isLawyerProfile: () => {
         return Template.instance().profileType.get() == 'Lawyer';
+    },
+    getAvatar: () => {
+        let user = Meteor.users.findOne({_id: Meteor.userId()});
+        if (user.profile.avatar) {
+            return user.profile.avatar;
+        } else {
+            return `<img align="right" class="profile-user-avatar" src="https://ui-avatars.com/api/?name=${user.profile.firstname}+${user.profile.lastname}&size=128&background=524763&color=FFF"/>`;
+        }
     }
 });
 
@@ -33,11 +41,17 @@ AutoForm.hooks({
     updateUserProfile: {
         onSuccess: () => {
             Notifications.success('Success', 'You have successfully updated your profile!');
+        },
+        onError: function(formType, error) {
+            Notifications.error('Error', error);
         }
     },
     updateLawyerProfile: {
         onSuccess: () => {
             Notifications.success('Success', 'You have successfully updated your lawyer profile!');
+        },
+        onError: function(formType, error) {
+            Notifications.error('Error', error);
         }
     }
 });
